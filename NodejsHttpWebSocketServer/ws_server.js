440ea72bf5f91c355c1293fd6539b14f0c113b6f
awei <awei@awei.com>
2020-09-17 19:26:54 +0800

    去掉websocket server的close和open函数，这两个函数用不到

--- NodejsHttpWebSocketServer/ws_server.js
+++ NodejsHttpWebSocketServer/ws_server.js
@@ -5,41 +5,29 @@ const server = new WebSocket.Server({ port: 8888 });
 var map_ws_userid = new Map();
 var ws_nums = 1;
 
-server.on('open', function open() {
-	console.log('connected');
-});
-
-server.on('close', function close() {
-	console.log('disconnected');
-});
-
 server.on('connection', function connection(ws, req) {
 	const ip = req.connection.remoteAddress;
 	const port = req.connection.remotePort;
 	const clientName = ip + port;
-
 	console.log('%s is connected', clientName)
-
 	// 发送欢迎信息给客户端
 	ws.send("Welcome " + clientName);
-
+	//接收到消息
 	ws.on('message', function incoming(message) {
+		var json = {"id":1,"arg":{"userid":300}}
 		console.log('received: %s from %s', message, clientName);
-		
 		//将userid ws加入map
 		ws_nums = ws_nums + 1;
 		map_ws_userid.set(ws_nums, ws);
 		console.log('add map_ws_userid size %d ws_nums %d', map_ws_userid.size, ws_nums);
-		
 		// 广播消息给所有客户端
 		server.clients.forEach(function each(client) {
 			if (client.readyState === WebSocket.OPEN) {
 				client.send( clientName + " -> " + message);
 			}
 		});
-
 	});
-
+	//客户端关闭调用
 	ws.on('close', function close() {
 		//将ws从map去掉
 		for (var item of map_ws_userid.entries()) {
@@ -52,5 +40,4 @@ server.on('connection', function connection(ws, req) {
 		}
 		console.log('close');
 	});
-
 });
