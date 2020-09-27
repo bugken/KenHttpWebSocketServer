ee3a861d3557fdc825f6c7a678d37ebc6a541f44
awei <awei@awei.com>
2020-09-27 22:23:38 +0800

    测试程序增加向服务端获取弹窗消息

--- NodejsHttpWebSocketServer/ws_client.js
+++ NodejsHttpWebSocketServer/ws_client.js
@@ -4,9 +4,9 @@ const sleep = require("sleep");
 var sockets = new Array()
 //测试临界值为1700个client
 new Promise(function(resolve, reject){
-    for(i = 0; i < 10000; i++)
+    for(i = 0; i < 1; i++)
 	{
-		socket = new WebSocket("ws://127.0.0.1:8888/ws");
+		var socket = new WebSocket("ws://127.0.0.1:8888/ws");
 		sockets.push(socket);
 		socket.onmessage = function (event) {
 			console.log("websocket client onmessage:%s.", event.data);
@@ -18,20 +18,30 @@ new Promise(function(resolve, reject){
 			console.log("websocket client onclose.");
 		};
 	}
+
     setTimeout(function(){
-        console.log("start send data. sockets size %d.", sockets.length);
 		var userid = 0;
-		var json = {"id":1, "arg":{"userid":userid}};
-		for(i = 0; i < sockets.length; i++)
+		var json_info = {"id":100001, "arg":{"userid":userid}};
+		var json_ask_message = {"id":100002, "arg":{"userid":userid}};
+		var number_sockets = sockets.length;
+		console.log("start send data. sockets size %d.", sockets.length);
+		for(i = 0; i < number_sockets; i++)
 		{
+			s = sockets.pop();
 			userid = userid + 1;
-			json = {"id":1, "arg":{"userid":userid}};
-			str = JSON.stringify(json);
-			if (socket.readyState == WebSocket.OPEN) {
+			json_info = {"id":100001, "arg":{"userid":userid}};
+			str = JSON.stringify(json_info);
+			if (s.readyState == WebSocket.OPEN) {
 				console.log("send data:%s", str);
-				socket.send(str);
+				s.send(str);
+			} 
+			json_ask_message = {"id":100002, "arg":{"userid":userid}};
+			str = JSON.stringify(json_ask_message);
+			if (s.readyState == WebSocket.OPEN) {
+				console.log("send data:%s", str);
+				s.send(str);
 			} 
 		}
         resolve('随便什么数据');
-    }, 20000);
+    }, 1000);
 });
