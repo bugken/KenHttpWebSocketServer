5515e3777697810396066bad18a5384190d9ecb9
awei <awei@awei.com>
2020-09-29 01:56:21 +0800

    1.调整编码换行格式 2.增加信息开关

--- NodejsHttpWebSocketServer/http_websocket_server.js
+++ NodejsHttpWebSocketServer/http_websocket_server.js
@@ -11,10 +11,11 @@ var g_map_ws_userid = new Map();
 var g_map_userid_login_message = new Map();
 var g_login_message_to_all = "";
 var g_maintenance_message = "";
+var g_log_switch = 1;
+var g_log_switch_http = 1;//只针对http的请求与回复消息
 
 //打印在线人数信息，包括在线人数数量和useid
-function dump_users_info()
-{
+function dump_users_info(){
 	console.log("g_ws_server.clients.size:%d", g_ws_server.clients.size);
 	console.log("g_map_ws_userid size:%d", g_map_ws_userid.size);
 	for (var item of g_map_ws_userid.entries()) {
@@ -22,8 +23,7 @@ function dump_users_info()
 	}
 }
 //读取登录信息、维护信息内容
-function dump_message()
-{
+function dump_message(){
 	console.log("maintenance message:%s", g_maintenance_message);
 	console.log("login message to all:%s", g_login_message_to_all);
 	for (var item of g_map_userid_login_message.entries()) {
@@ -31,160 +31,129 @@ function dump_message()
 	}
 }
 //清空登录信息，维护信息
-function clear_message()
-{
+function clear_message(){
 	g_maintenance_message = "";
 	g_login_message_to_all = "";
 	g_map_userid_login_message.clear();
 }
-function dump_login_messages()
-{
-	console.log("login message size:%d", array_login_messages.length());
-	//遍历message
+//打印信息开关
+function log_switch(json_data){
+	var ret = 0;
+	var error_message = "";
+	if(json_data.operation == 0)//关闭log
+		g_log_switch = 0;
+	else if(json_data.operation == 1)
+		g_log_switch = 1;
+	if(json_data.op_http == 0)//http收发消息开关
+		g_log_switch_http = 0;
+	else if(json_data.op_http == 1)
+		g_log_switch_http = 1;
+	var json = {"ret":ret, "error_message":error_message};
+	return json;
 }
 //在线人数查询
-function query_users_online()
-{
+function query_users_online(){
 	//var counts = g_ws_server.clients.size;
 	var counts = g_map_ws_userid.size;
 	var json = {"ret":0, "users_online":counts};
 	return json;
 }
 //广播数据给用户 type 1:弹窗消息 2:维护消息
-function broadcast_message(type, message)
-{
-	for (var item of g_map_ws_userid.entries()) 
-	{
+function broadcast_message(type, message){
+	for (var item of g_map_ws_userid.entries()) {
 		json = {"id":200001, "arg":{"type":type, "message": message}};  //200001:客户端弹窗消息
 		str = JSON.stringify(json);
-		console.log("send to: %d msg: %s", item[0], str);
+		if (g_log_switch == 1)
+			console.log("send to: %d msg: %s", item[0], str);
 
 		item[1].send(str);
 	}
 }
 //发送消息给特定玩家 type 1:弹窗消息 2:维护消息
-function notify_message(userid, type, message)
-{
+function notify_message(userid, type, message){
 	var ret = 0;
-	if(g_map_ws_userid.has(userid))
-	{
+	if(g_map_ws_userid.has(userid)){
 		var ws = g_map_ws_userid.get(userid);
 		json = {"id":200001, "arg":{"type":type, "message":message}};  
 		str = JSON.stringify(json);
-		console.log("send to: %d msg: %s", userid, str);
+		if (g_log_switch == 1)
+			console.log("send to: %d msg: %s", userid, str);
 
 		ws.send(str);
 	}
-	else
-	{
-		console.log("userid %d 不在线", userid);
+	else{
+		if (g_log_switch == 1)
+			console.log("userid %d 不在线", userid);
 		ret = -1;
 	}
 	return ret;
 }
 //发送数据
-function send_message(json_data)
-{
+function send_message(json_data){
 	var ret = 0;
 	var error_message = "";
-	if(json_data.userid > 0)
-	{
-		//发送消息给特定玩家玩家
+	if(json_data.userid > 0){//发送消息给特定玩家玩家
 		var ret = 0;
 		ret = notify_message(json_data.userid, 1, json_data.message);
 		if(ret == -1)
-		{
 			error_message = util.format("userid:%d 不在线", json_data.userid);
-		}
 	}
-	else if(json_data.userid == 0)
-	{
-		//广播消息
+	else if(json_data.userid == 0)//广播消息
 		broadcast_message(1, json_data.message);
-	}
-	else
-	{
+	else{
 		ret = -1;
 		error_message = util.format("userid %d 不正确", json_data.userid);
 	}
 	var json = {"ret":ret, "error_message":error_message};
 	return json;
 }
-//保存登录信息
-function save_login_message(json_data)
-{
+//保存或清除登录信息
+function save_login_message(json_data){
 	var ret = 0;
 	var error_message = "";
-	if(json_data.userid > 0)
-	{		
+	if(json_data.userid > 0){		
 		if(json_data.type == 0)
-		{
 			g_map_userid_login_message.delete(json_data.userid);
-		}
 		else//保存登录消息
-		{
 			g_map_userid_login_message.set(json_data.userid, json_data.message);
-		}
 	}
-	else if(json_data.userid == 0)
-	{
+	else if(json_data.userid == 0){
 		if(json_data.type == 0)
-		{
 			g_login_message_to_all = "";
-		}
-		else//保存登录消息
-		{
+		else
 			g_login_message_to_all = json_data.message;
-		}
 	}
-	else
-	{
+	else{
 		ret = -1;
 		error_message = util.format("userid %d 不正确", json_data.userid);
 	}
 	var json = {"ret":ret, "error_message":error_message};
 	return json;
-}
-//删除登录消息
-function delete_login_message(json_data)
-{
-	//var pos = fruits.indexOf('Banana');
-	//var removedItem = fruits.splice(pos, 1); // this is how to remove an item
 }
 //处理登录与弹框消息
-function handle_pop_login_message(json_data)
-{
+function handle_pop_login_message(json_data){
 	var ret = 0;
 	var error_message = "";
-	if(json_data.userid > 0)
-	{
+	if(json_data.userid > 0){
 		if(json_data.type == 0)
-		{
 			g_map_userid_login_message.delete(json_data.userid);
-		}
-		else
-		{
+		else{
 			//保存到特定玩家的登录消息
 			g_map_userid_login_message.set(json_data.userid, json_data.message);
 			//弹框提示
 			notify_message(json_data.userid, 1, json_data.message);
 		}
 	}
-	else if(json_data.userid == 0)
-	{
+	else if(json_data.userid == 0){
 		if(json_data.type == 0)
-		{
 			g_login_message_to_all = "";
-		}
-		else
-		{
+		else{
 			g_login_message_to_all = json_data.message;
 			//弹窗提示
 			broadcast_message(1, json_data.message);
 		}
 	}
-	else
-	{
+	else{
 		ret = -1;
 		error_message = util.format("userid %d 不正确", json_data.userid);
 	}
@@ -193,7 +162,7 @@ function handle_pop_login_message(json_data)
 }
 /*****************************************http server************************************************/
 //监听端口
-http_server.listen(g_http_server_port, "0.0.0.0");
+g_http_server.listen(g_http_server_port, "0.0.0.0");
 //设置超时时间
 g_http_server.setTimeout(5 * 60 * 1000);
 //服务器监听时触发
@@ -210,59 +179,49 @@ g_http_server.on("request", function (req, res) {
 		data += chunk;  
 	}); 
 	req.on("end",function(){  
-		console.log("http received data from web:%s", data);
-		try
-		{
+		if (g_log_switch_http == 1)
+			console.log("http received data from web:%s", data);
+		try{
 			datajson = JSON.parse(data);  
-			if(datajson.id == 900001)//打印在线人数信息，包括在线人数数量和useid
-			{
+			if(datajson.id == 900001){//打印在线人数信息，包括在线人数数量和useid
 				dump_users_info();
 				retStr = {"ret":0, "error_message":""};
 			}
-			if(datajson.id == 900002)//读取登录信息、维护信息内容
-			{
+			if(datajson.id == 900002){//读取登录信息、维护信息内容
 				dump_message();
 				retStr = {"ret":0, "error_message":""};
 			}
-			if(datajson.id == 900003)//读取登录信息、维护信息内容
-			{
+			if(datajson.id == 900003){//读取登录信息、维护信息内容
 				clear_message();
 				retStr = {"ret":0, "error_message":""};
 			}
+			if(datajson.id == 900004){//日志开关
+				log_switch(datajson.arg);
+				retStr = {"ret":0, "error_message":""};
+			}
 			else if(datajson.id == 100001)//在线人数
-			{
 				retStr = query_users_online();
-			}
 			else if(datajson.id == 100002)//发送弹窗消息
-			{
 				retStr = send_message(datajson.arg);
-			}
 			else if(datajson.id == 100003)//保存登录消息
-			{
 				retStr = save_login_message(datajson.arg);
-			}
-			else if(datajson.id == 100004)//维护消息
-			{
-				if(datajson.arg.type == 1)
-				{
+			else if(datajson.id == 100004){//维护消息
+				if(datajson.arg.type == 1){
 					g_maintenance_message = datajson.arg.message;
 					broadcast_message(2, g_maintenance_message);
 				}
-				else if(datajson.arg.type == 0)
-				{
+				else if(datajson.arg.type == 0){
 					g_maintenance_message = "";
 				}
 				retStr = {"ret":0, "error_message":""};
 			}
 			else if(datajson.id == 100005)//登录与弹窗消息
-			{
 				retStr = handle_pop_login_message(datajson.arg);
-			}
-			console.log("reply to web:%s", JSON.stringify(retStr));
+			if (g_log_switch_http == 1)
+				console.log("reply to web:%s", JSON.stringify(retStr));
 			res.end(JSON.stringify(retStr));
 		}
-		catch(e)
-		{
+		catch(e){
 			console.log("Exception error:%s", e.message);
 			res.end(JSON.stringify(retStr));
 		}
@@ -270,17 +229,14 @@ g_http_server.on("request", function (req, res) {
 });
 //连接建立时触发
 g_http_server.on("connection", function (socket) {
-	//console.log('建立连接');
 	return;
 });
 //客户端向服务器发送CONNECT请求时触发
 g_http_server.on("connect", function (req, socket, head) {
-	//console.log('客户端connect');
 	return;
 });
 //服务器关闭时触发，调用 close() 方法。
 g_http_server.on("close", function () {
-	//console.log('服务器关闭');
 	return;
 });
 //发生错误时触发
@@ -289,32 +245,28 @@ g_http_server.on("error", function (err) {
 });
 //如果连接超过指定时间没有响应，则触发。超时后，不可再复用已建立的连接，需发请求重新建立连接
 g_http_server.on("timeout", function (socket) {
-	//console.log("连接已超时");
 	return;
 });
 
 /*****************************************websocket server************************************************/
 //针对userid == 0的发送函数
-function ws_notify_message(ws)
-{
+function ws_notify_message(ws){
 	var type = 0;
 	var message = "";
-	if(g_login_message_to_all != "")
-	{
+	if(g_login_message_to_all != ""){
 		type = 1;
 		message = g_login_message_to_all;
 	}
-	if(g_maintenance_message != "")//维护消息优先级比较高
-	{
+	if(g_maintenance_message != ""){//维护消息优先级比较高
 		type = 2;
 		message = g_maintenance_message;
 	}
 
-	if(message != "")
-	{
+	if(message != ""){
 		json = {"id":200001, "arg":{"type":type, "message":message}};
 		str = JSON.stringify(json);
-		console.log("send to: %d msg: %s", 0, str);
+		if (g_log_switch == 1)
+			console.log("send to: %d msg: %s", 0, str);
 		ws.send(str);
 	}
 }
@@ -326,54 +278,38 @@ g_ws_server.on("listening", function listen(){
 g_ws_server.on("connection", function connection(ws, req) {
 	//接收到消息
 	ws.on('message', function incoming(message) {
-		try
-		{
-			console.log("ws receive message:%s", message);
+		try{
+			if (g_log_switch == 1)
+				console.log("ws receive message:%s", message);
 			datajson = JSON.parse(message);  
 			//客户端向服务端发送的第一条消息，告诉服务端用户的信息
-			if(datajson.id == 100001)
-			{
+			if(datajson.id == 100001){
 				var userid = datajson.arg.userid;
-				if(g_maintenance_message != "")
-				{
+				if(g_maintenance_message != ""){
 					ws_notify_message(ws);
 					return;
 				}
 				if(userid == 0)
-				{
 					ws_notify_message(ws);
-				}
-				else
-				{
-					//将userid ws加入map
+				else//将userid ws加入map
 					g_map_ws_userid.set(userid, ws);
-				}
 			}
 			//向服务端请求弹窗信息
-			else if(datajson.id == 100002)
-			{
+			else if(datajson.id == 100002){
 				var userid = datajson.arg.userid;
 				if(userid == 0)
-				{
 					ws_notify_message(ws);
-				}
-				if(g_maintenance_message != "")
-				{
+				if(g_maintenance_message != ""){
 					notify_message(userid, 2, g_maintenance_message);
 					return;
 				}
 				if(g_map_userid_login_message.has(userid))//针对特定用户的登录弹窗
-				{
 					notify_message(userid, 1, g_map_userid_login_message.get(userid));
-				}
 				if(g_login_message_to_all != "")
-				{
 					notify_message(userid, 1, g_login_message_to_all);
-				}
 			}
 		}
-		catch(e)
-		{
+		catch(e){
 			console.log("Exception error:%s", e.message);
 		}
 	});
@@ -381,10 +317,10 @@ g_ws_server.on("connection", function connection(ws, req) {
 	ws.on("close", function close() {
 		//将ws从map去掉
 		for (var item of g_map_ws_userid.entries()) {
-			if(item[1] == ws)
-			{
+			if(item[1] == ws){
 				g_map_ws_userid.delete(item[0]);
-				console.log("delete userid %d, g_map_ws_userid size %d", item[0], g_map_ws_userid.size);
+				if (g_log_switch == 1)
+					console.log("delete userid %d, g_map_ws_userid size %d", item[0], g_map_ws_userid.size);
 			}
 		}
 	});
